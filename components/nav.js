8857dae4e332fbb26823ad8cd9323452ae230141
Maliha Hossain <malu.hoss@gmail.com>
2020-12-06 18:22:33 -0500

    Navigation - add title, sections, brand colours & font

--- components/nav.js
+++ components/nav.js
@@ -1,25 +1,26 @@
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-  { href: 'https://nextjs.org/docs', label: 'Docs' },
+  { href: 'http://localhost:3000/about', label: 'about' },
+  { href: 'http://localhost:3000/episodes', label: 'episodes' },
+  { href: 'http://localhost:3000/contact', label: 'send us a message' }
 ]
 
 export default function Nav() {
   return (
-    <nav>
-      <ul className="flex items-center justify-between p-8">
+    <nav className="bg-white shadow-md px-16 py-5">
+      <ul className="flex items-center justify-between">
         <li>
           <Link href="/">
-            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
-              Home
+            <a className="text-grey no-underline font-bold tracking-widest text-4xl">
+              bengali boost
             </a>
           </Link>
         </li>
-        <ul className="flex items-center justify-between space-x-4">
+        <ul className="flex items-center justify-between space-x-10">
           {links.map(({ href, label }) => (
             <li key={`${href}${label}`}>
-              <a href={href} className="no-underline btn-blue">
+              <a href={href} className="text-grey no-underline btn-blue font-bold tracking-widest text-xl">
                 {label}
               </a>
             </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,24 @@ module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // 'media' or 'class'
   theme: {
+    fontFamily: {
+      body: ['Montserrat', 'sans-serif'],
+      display: ['Montserrat', 'sans-serif']
+    },
     extend: {
       colors: {
-        'accent-1': '#333',
+        indigo: '#581845',
+        rubyRed: '#900C3F',
+        red: '#C70039',
+        orange: '#FF5733',
+        yellow: '#FFC30F',
+        grey: '#363636',
+        white: '#FFFFFF'
       },
-    },
+      height: theme => ({
+        'adjust': 'calc(100vh - 87.62px - 87.62px)',
+      })
+    }
   },
   variants: {
     extend: {},
