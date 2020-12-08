39177da1c5b809e9218c984d12050c336a9cb077
Maliha Hossain <malu.hoss@gmail.com>
2020-12-07 22:01:53 -0500

    App/home - widen spacing between text and button on homepage

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function IndexPage() {
         <Nav />
         <div className="h-adjust flex py-6 px-16 space-x-4">
           <img src="/images/girl-listening.svg" className="max-w-full h-full w-3/6" />
-          <div className="flex flex-col justify-center space-y-6 py-60">
+          <div className="flex flex-col justify-center space-y-10 py-60">
             <h1 className="tracking-wide text-5xl text-grey font-bold">
               discover the untold stories of Bengalis who are changing the world
             </h1>
