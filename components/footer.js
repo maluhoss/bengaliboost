1f18e39d7ce08074be8536b39f95df0faf66b3d2
Maliha Hossain <malu.hoss@gmail.com>
2020-12-11 00:12:54 -0500

    App/footer - add white logo images if background is not yellow

--- components/footer.js
+++ components/footer.js
@@ -1,21 +1,44 @@
 import Link from 'next/link'
 
 const links = [
-  { href: '#', label: 'Apple', image: '/images/apple-logo.svg' },
-  { href: '#', label: 'Spotify', image: '/images/spotify-logo.svg' },
+  {
+    href: '#',
+    label: 'Apple',
+    image: {
+      dark: '/images/apple-logo.svg',
+      light: '/images/apple-logo-white.svg'
+    },
+  },
+  {
+    href: '#',
+    label: 'Spotify',
+    image: {
+      dark: '/images/spotify-logo.svg',
+      light: '/images/spotify-logo-white.svg'
+    }
+  }
 ]
 
 export default function Footer({ backgroundColour }) {
-  const footerTextColour = (backgroundColour !== 'bg-yellow') ? 'text-white' : 'text-grey'
+  let footerTextColour
+  let logo
 
+  if (backgroundColour !== 'bg-yellow') {
+    footerTextColour = 'text-white'
+    logo = 'light'
+  } else {
+    footerTextColour = 'text-grey'
+    logo = 'dark'
+  }
+  
   return (
-    <footer className={`${backgroundColour} flex justify-center py-2 space-x-6 h-8`}>
-        <p className={`font-bold ${footerTextColour} tracking-widest`}>listen on: </p>
-        <ul className={`${footerTextColour} flex items-center space-x-8`}>
+    <footer className={`${backgroundColour} flex justify-center space-x-2 sm:space-x-6 h-8`}>
+        <p className={`text-lg font-bold ${footerTextColour} tracking-widest`}>listen on: </p>
+        <ul className={`${footerTextColour} flex items-center space-x-4 sm:space-x-8`}>
           {links.map(({ href, label, image }) => (
             <li key={`${href}${label}`}>
               <div className="flex space-x-2">
-                <img src={image} width="24px" />
+                <img src={image[logo]} width="20px" />
                 <a href={href} className={`${footerTextColour} no-underline font-bold`}>
                   {label}
                 </a>
