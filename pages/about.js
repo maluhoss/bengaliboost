0c591d55d35c0d977e6f91cef92143fc086044cd
Maliha Hossain <malu.hoss@gmail.com>
2020-12-12 18:54:29 -0500

    App/about - remove unused code and add margin beside image

--- pages/about.js
+++ pages/about.js
@@ -16,41 +16,10 @@ export default function About() {
               we are a bunch of kids in Toronto that got super frustrated by the incredibly annoying opinions and negativity shown by Bengalis to other Bengalis so we decided to create a podcast that highlighted the amazingly cool things that Bengalis were doing so we can all be inspired and empowered and create the Bengali culture we wish to see in the world
               </p>
             </div>
-            <img src="/images/girl-listening.svg" className="m-auto bg-white min-w-image-sm sm:min-w-image-lg w-40 h-40 sm:h-2/6 sm:w-2/6" />
+            <img src="/images/girl-listening.svg" className="sm:ml-4 m-auto bg-white min-w-image-sm sm:min-w-image-lg w-40 h-40 sm:h-2/6 sm:w-2/6" />
           </div>
         </div>
       <Footer backgroundColour={backgroundColour} />
-      {/* <Nav />
-        <div className={`w-full h-screen px-10 py-6 ${backgroundColour}`}>
-          <div className="flex flex-wrap space-x-10 space-y-10 sm:space-y-0 sm:flex-nowrap sm:py-10">
-            <div className="flex flex-col justify-center space-y-5 mx-auto">
-              <h1 className="text-center sm:text-left max-w-sm sm:max-w-md tracking-widest leading-tight text-xl sm:text-3xl md:text-4xl lg:text-4xl text-yellow font-bold">
-                about
-              </h1>
-              <p className="mx-auto text-center sm:text-left max-w-sm sm:max-w-sm tracking-widest leading-tight text-sm sm:text-base md:text-xl text-white font-bold">
-              we are a bunch of kids in Toronto that got super frustrated by the incredibly annoying opinions and negativity shown by Bengalis to other Bengalis so we decided to create a podcast that highlighted the amazingly cool things that Bengalis were doing so we can all be inspired and empowered and create the Bengali culture we wish to see in the world
-              </p>
-            </div>
-            <img src="/images/girl-listening.svg" className="bg-white mx-auto w-64 sm:w-2/4" />
-          </div>
-        </div>
-      <Footer backgroundColour={backgroundColour} /> */}
-
-      {/* <div>
-        <Nav />
-        <div className={`h-adjust flex py-6 px-16 space-x-4 ${backgroundColour}`}>
-          <div className="flex flex-col justify-end space-y-10 py-60">
-            <h1 className="tracking-wide text-4xl text-yellow font-bold">
-              about
-            </h1>
-            <p className="text-white font-bold">
-            
-            </p>
-          </div>
-          <img src="/images/girl-listening.svg" className="max-w-full h-full w-3/6 bg-yellow rounded-3xl" />
-        </div>
-        <Footer backgroundColour={backgroundColour} />
-      </div> */}
     </>
   )
 }
