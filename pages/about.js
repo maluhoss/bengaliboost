17facd2ae7b94b692af92ce61d154b2e715cfc0e
Maliha Hossain <malu.hoss@gmail.com>
2020-12-18 22:19:17 -0500

    App/about - revise content & adjust font size

--- pages/about.js
+++ pages/about.js
@@ -12,11 +12,11 @@ export default function About() {
               <h1 className="text-center sm:text-left max-w-md tracking-widest leading-tight text-3xl md:text-4xl lg:text-5xl text-yellow font-bold">
                 about
               </h1>
-              <p className="mx-auto text-center sm:text-left max-w-md tracking-widest leading-tight text-sm md:text-base lg:text-2xl text-white font-bold">
+              <p className="mx-auto text-center sm:text-left max-w-md tracking-widest leading-tight text-sm md:text-base lg:text-xl text-white font-bold">
                 Bengali Boost is all about boosting 🚀 the people, stories, parts of our culture that inspire us.
               </p>
               <p className="mx-auto text-center sm:text-left max-w-md tracking-widest leading-tight text-xs md:text-xs lg:text-sm text-white font-bold">
-                Bengali Boost was born in the weird times of 2020 when we found ourselves searching for community, masks and sanitizer, forgetting we were on mute, grappling with big issues like health, isolation, racism, and trying to figure out how our culture both helps us and hinders us. Despite the tough times we were inspired by the artists, innovators, storytellers and brave Bengalis who were and still are changing the world. Bengali Boost is about them and it's about figuring out what a future Bengali-diaspora culture could look like.
+              Bengali Boost was born in the weird times of 2020. After we stopped impulse-buying masks, sanitizer and toilet paper in bulk, we realized what we actually needed most was community. We were also grappling with big issues like health, racism, civic duty and trying to figure out how our culture fits into it all. Despite the tough times we were inspired by the artists, innovators, storytellers and brave Bengalis making waves in the world. Bengali Boost is about them, about connecting us all together and about figuring out what a future Bengali-diaspora culture could look like.
               </p>
             </div>
             <img src="/images/crowd.svg" className="sm:ml-4 m-auto min-w-image-sm sm:min-w-image-lg w-32 h-32 sm:h-2/6 sm:w-2/6" />
