59c58b63415e2f646d07ddd7a07bc71f7ea2c619
Maliha Hossain <malu.hoss@gmail.com>
2021-01-02 18:38:26 -0500

    App - fix favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="/images/favicon.png" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
