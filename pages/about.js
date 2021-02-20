0ea79c323bb3b9a38259dbdc0e428572270c4eea
Maliha Hossain <malu.hoss@gmail.com>
2021-02-20 02:06:05 -0500

    About - change copy to include definition

--- pages/about.js
+++ pages/about.js
@@ -17,10 +17,13 @@ export default function About() {
                 about
               </h1>
               <p className="text-center sm:text-left max-w-md tracking-widest leading-tight text-base lg:text-xl text-white font-bold">
-              We are a hub for Bengali artists.
+              We are a hub for Bengali artists*.
               </p>
               <p className="mx-auto text-center sm:text-left max-w-md tracking-widest leading-tight text-sm md:text-sm lg:text-base text-white font-semibold">
               We want to see more Bengalis make great stuff so we want to 1) make it easier for artists to learn from each other and 2) feel like there's a community  where they can share, make mistakes, get feedback, collab, learn, find support and just have fun. <br/><br/> Whether you've been making art for a while or just getting started, there's something here for you. 🎉
+              </p>
+              <p className="mx-auto text-center sm:text-left max-w-md tracking-widest leading-tight text-xs md:text-xs lg:text-sm text-white font-semibold">
+              *Our definition of an artist is someone who adds their expression to something and it can be shared with the world
               </p>
             </div>
             <img src="/images/crowd.svg" className="sm:ml-4 m-auto min-w-image-sm sm:min-w-image-lg w-32 h-32 sm:h-2/6 sm:w-2/6" />
