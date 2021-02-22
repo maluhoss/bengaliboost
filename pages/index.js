946329f829c4ea16c3c3c4ccf13ce533adbb1382
Maliha Hossain <malu.hoss@gmail.com>
2021-02-21 20:42:13 -0500

    Home - fix image size

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function IndexPage({ episodes }) {
       </Head>
       <Nav />
       <div className={`w-full h-adjust overflow-auto px-10 ${backgroundColour}`}>
-        <div className="flex mx-auto justify-center content-center min-h-adjust flex-wrap space-y-4 sm:space-y-10 lg:space-y-0 sm:flex-nowrap sm:py-10">
+        <div className="flex mx-auto justify-center content-center min-h-adjust flex-wrap space-y-4 sm:space-y-10 lg:space-y-0 sm:flex-nowrap sm:py-5">
           <img src="/images/rocket.gif" className="m-auto w-2/5 h-2/5 min-w-image-sm sm:h-1/6 sm:w-1/6" />
           <div className="flex flex-col justify-center space-y-5 sm:space-y-10 mx-auto">
             <h1 className="mx-auto text-center md:text-left max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg tracking-widest leading-tight text-xl sm:text-2xl md:text-4xl lg:text-5xl text-grey font-bold">
